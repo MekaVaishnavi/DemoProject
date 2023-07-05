7c74585d6428159133624cb2749d0b4ff18f74ad
MekaVaishnavi <vaishnavimeka.99@gmail.com>
2023-07-05 14:34:09 -0700

    Added Two Sum file

---
+++ Two Sum/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+	n := len(nums)
+	for i := 0; i < n-1; i++ {
+		for j := i + 1; j < n; j++ {
+			if target == nums[i]+nums[j] {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}
+
+func main() {
+	nums := []int{2, 7, 11, 15}
+	target := 9
+
+	result := twoSum(nums, target)
+	fmt.Println(result)
+}
